4c0e7a4bf00a76455d17ded2fdba51a2f23042f1
Ailton Duarte <adidas.coyas@gmail.com>
2021-03-27 16:07:39 -0100

    check

--- componentes/NavBar/index.tsx
+++ componentes/NavBar/index.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 
 const NavBar = () => {
 	return (
-		// Copied!
-
 		<header className="text-gray-600 body-font">
 			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
